883ec1ac02a948f5d2bdbd109f4a927c4f4e1b96
eRBeGe <adryanpaw853@gmail.com>
2023-02-24 14:52:06 +0700

    view login merchant jadi gan

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: LoginPage(),
     );
   }
 }
