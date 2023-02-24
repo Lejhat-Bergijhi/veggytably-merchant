92ff61266a8cf1ed0d7d1a8ca343feaa36dc6e0b
eRBeGe <adryanpaw853@gmail.com>
2023-02-24 12:58:10 +0700

    create login n signup pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'views/login_page.dart';
+import 'views/signup_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,6 +12,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
