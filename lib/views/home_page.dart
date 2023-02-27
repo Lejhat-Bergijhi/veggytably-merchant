2b18e1ecdfd24081541d7b1549ffd59788eddcf2
Delfia NAP <delfia.nur2004@mail.ugm.ac.id>
2023-02-27 12:33:59 +0700

    homepage untuk navigasi ke profil

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -14,66 +14,148 @@ class HomePage extends StatelessWidget {
             Container(
                 width: 350,
                 height: 300,
-                color: Colors.teal[300],
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                            Color(0xFF7BD896),
+                            Color(0xFF60B874)
+                        ],                                                                         
+                    ),  
+                )
 
             ),
 
-            
         ),
-        
-        //appbar invisible
+
         appBar: AppBar(
-            //larger bar
-            toolbarHeight: 80.0,
+            flexibleSpace: Container(
+                decoration: const BoxDecoration(
+                     borderRadius: BorderRadius.only(                                           
+                        bottomRight: Radius.circular(16), bottomLeft: Radius.circular(16)
+                    ),
+                    gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                            Color(0xFF7BD896),
+                            Color(0xFF60B874)
+                        ],  
+                    ),
+                ),
+            ),
+            toolbarHeight: 115.0,
             elevation: 0,
-             actions: <Widget>[
+            actions: <Widget>[
                 
                 
                 IconButton(
-                    icon: const Icon(Icons.settings),
+                    //profile icon
+
+                    //make icon larger
+                    iconSize: 45.0,
+                    icon: const Icon(Icons.account_circle),
                     onPressed: () {
                         // do something
                     },
                     color: Colors.black,
                 ),
                 //spacing
-                const SizedBox(width: 10.0),
-                IconButton(
-                    icon: const Icon(Icons.logout),
-                    onPressed: () {
-                        // do something
-                        
-                    },
-                    color: Colors.black,
-                ),
-                const SizedBox(width: 10.0),
+                const SizedBox(width: 20.0),
+
             ],
         ),
 
-        bottomNavigationBar: BottomNavigationBar(
-            items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-                icon: Icon(Icons.home_rounded),
-                label: 'Home',
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.local_dining_rounded),
-                label: 'Orders',
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.menu_book_rounded),
-                label: 'Menu',
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.wallet),
-                label: 'Domvet',
-            ),
-            ],
-            // currentIndex: _selectedIndex,
-            // onTap: _onItemTapped,
-            selectedItemColor: Colors.teal[300],
-        ),
 
+        // appBar: Container(
+        //     height: 80,
+        //     decoration: BoxDecoration(
+        //         gradient: LinearGradient(
+        //             begin: Alignment.topLeft,
+        //             end: Alignment.bottomRight,
+        //             colors: [
+        //                 Color(0xFF7BD896),
+        //                 Color(0xFF60B874)
+        //             ],                                                                         
+        //         ),  
+        //     ),
+        //     child: Row(
+        //         children: [
+        //             IconButton(
+        //                 icon: Icon(Icons.account_circle),
+        //                 onPressed: () {
+        //                     // do something
+        //                 },
+        //                 color: Colors.black,
+        //             ),
+        //             Text("Hello World"),
+        //         ],
+        //     ),
+        // ),
+        // //appbar invisible
+        // appBar: AppBar(
+        //     //larger bar
+        //     toolbarHeight: 80.0,
+        //     elevation: 0,
+        //      actions: <Widget>[
+                
+                
+        //         IconButton(
+        //             //profile icon
+
+
+        //             icon: const Icon(Icons.account_circle),
+        //             onPressed: () {
+        //                 // do something
+        //             },
+        //             color: Colors.black,
+        //         ),
+        //         //spacing
+        //         const SizedBox(width: 10.0),
+
+        //     ],
+        // ),
+        bottomNavigationBar: Container(                                             
+            decoration: BoxDecoration(                                                   
+                borderRadius: BorderRadius.only(                                           
+                topRight: Radius.circular(16), topLeft: Radius.circular(16)),            
+                boxShadow: [                                                               
+                BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 20),       
+                ],
+
+            ),                                                                         
+            child: ClipRRect(                                                            
+                borderRadius: BorderRadius.only(                                           
+                    topLeft: Radius.circular(16.0),                                            
+                    topRight: Radius.circular(16.0),                                           
+                ),                                                                         
+                child: BottomNavigationBar(
+                    items: const <BottomNavigationBarItem>[
+                        BottomNavigationBarItem(
+                            icon: Icon(Icons.home_rounded),
+                            label: 'Home',
+                        ),
+                        BottomNavigationBarItem(
+                            icon: Icon(Icons.local_dining_rounded),
+                            label: 'Orders',
+                        ),
+                        BottomNavigationBarItem(
+                            icon: Icon(Icons.menu_book_rounded),
+                            label: 'Menu',
+                        ),
+                        BottomNavigationBarItem(
+                            icon: Icon(Icons.wallet),
+                            label: 'Domvet',
+                        ),
+                    ],
+                    // currentIndex: _selectedIndex,
+                    // onTap: _onItemTapped,
+                    selectedItemColor: Colors.teal[300],
+                ),                                                                     
+            )                                                                           
+        )
+    
         //
     );
   }
