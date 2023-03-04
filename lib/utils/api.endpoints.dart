d10b11966fe5424e95fd52bfa74cc4cf3f1d9590
jon-mun <jonathan.edmund@gmail.com>
2023-03-04 13:02:56 +0700

    update api endpoint

--- lib/utils/api.endpoints.dart
+++ lib/utils/api.endpoints.dart
@@ -1,15 +1,15 @@
 class ApiEndPoints {
-  static const String baseUrl = "http://localhost:5000/merchants/";
+  static const String baseUrl = "http://localhost:5000/";
   static final _AuthEndPoints authEndpoints = _AuthEndPoints();
   static final _MerchantEndPoints merchantEndpoints = _MerchantEndPoints();
 }
 
 class _AuthEndPoints {
-  final String signUp = "auth/signUp";
+  final String signUp = "auth/signUp/merchant";
   final String login = "auth/login";
   final String logout = "auth/logout";
 }
 
 class _MerchantEndPoints {
-  final String getProfile = "profile";
+  final String getProfile = "merchants/profile";
 }
