12e9d324c289f4471c4498ca261213a7ce4e4005
unknown <sanialubba10@gmail.com>
2023-03-15 13:23:40 +0700

    update login + signup page 2

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'views/login_page.dart';
 import 'views/signup_page.dart';
+import 'views/signup_page_2.dart';
 import 'views/home_page.dart';
 import 'views/profile_page.dart';
 
@@ -17,7 +18,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: SignUpPage(),
     );
   }
 }
