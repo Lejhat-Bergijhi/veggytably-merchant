84b4641944c05fd567e0c18cee6033b42f2be393
Delfia NAP <delfia.nur2004@mail.ugm.ac.id>
2023-03-16 17:04:03 +0700

    profile page from user app unmodified

--- lib/views/profile_page.dart
+++ lib/views/profile_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:vegytably_merchant/views/login_page.dart';
+// import 'package:vegytably_merchant/views/restriction_page1.dart';
+import 'package:vegytably_merchant/widgets/bottom_navbar.dart';
+import 'package:vegytably_merchant/widgets/profile_menu.dart';
+import 'package:vegytably_merchant/views/edit_profile.dart';
 
-import '../controllers/auth_controller.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -10,223 +13,150 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: Column(
-          //
-          children: [
-            Container(
-                padding: const EdgeInsets.only(top: 17.0, left: 17.0),
-                child: Row(
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.arrow_back),
-                      onPressed: () {
-                        Get.back();
-                      },
+      backgroundColor: Colors.white,
+      body: Column(
+        
+        children: [
+          Column(
+            children: [
+              SizedBox(height: 67,),
+              Stack(
+                children: [Container(
+                  height:100,
+                  width: 100,
+                  child: Image.asset('images/Profpic.png'),
+                ),
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: InkWell(
+                    onTap: () {
+                      // showModal();
+                    },
+                    child: const Icon(
+                      Icons.camera_alt,
+                      color: Color.fromARGB(255, 23, 47, 44),
+                      size: 28,
                     ),
-                    const SizedBox(width: 7.0),
-                    Container(
-                      child: const Text(
-                        "Profile",
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    )
-                  ],
-                )),
-            const SizedBox(height: 20.0),
-            Container(
-                height: 205,
-                padding: const EdgeInsets.only(left: 78, right: 78),
-                child: Container(
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          color: Colors.black,
-                          Icons.account_circle_rounded,
-                          size: 70,
-                        ),
-                        const SizedBox(
-                          height: 12,
-                        ),
-                        Container(
-                          child: Text(
-                            "Nandarelle Aurora Trevor",
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 17,
-                              fontFamily: "Rubik",
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 6,
-                        ),
-                        Container(
-                            child: Text(
-                          "+62 89101112131",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 15,
-                            fontFamily: "Rubik",
-                            fontWeight: FontWeight.w400,
-                          ),
-                        )),
-                      ]),
-                )),
-            const SizedBox(height: 20.0),
-            Container(
-                child: ListView(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              scrollDirection: Axis.vertical,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(
-                        color: Colors.black12,
-                        width: 1.0,
-                      ),
-                      top: BorderSide(
-                        color: Colors.black12,
-                        width: 1.0,
-                      ),
-                    ),
-                  ),
-                  padding: const EdgeInsets.only(
-                      left: 24.0, right: 24.0, top: 10, bottom: 10),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.edit,
-                        color: Colors.black12,
-                        size: 40,
-                      ),
-                      const SizedBox(width: 17.0),
-                      Container(
-                        child: const Text(
-                          "Edit Profile",
-                          style: TextStyle(
-                            fontSize: 17.0,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ),
-                      const Spacer(),
-                      Container(
-                        child: const Icon(Icons.arrow_forward_ios,
-                            size: 17, color: Color(0xFF5F5F5F)),
-                      )
-                    ],
                   ),
                 ),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(
-                        color: Colors.black12,
-                        width: 1.0,
-                      ),
-                      // top: BorderSide(
-                      //   color: Colors.black12,
-                      //   width: 1.0,
-                      // ),
+                ]
+              ),
+              SizedBox(height: 14,),
+              Text(
+                  "NadiahhhHasna",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Color(0xff242424),
+                      fontSize: 22,
+                  ),
+              ),
+              SizedBox(height: 7,),
+              Text(
+                  "+6234567891011",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Color(0xff6482a6),
+                      fontSize: 12,
+                  ),
+              ),
+              SizedBox(height: 61,),
+              
+
+              ListView(
+                shrinkWrap: true,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: (){
+                      Get.offAll(() => EditProfile(), transition: Transition.fade);
+                    }, 
+                    child: ProfileMenu(
+                      "images/editprofile.png",
+                      "Edit Profile",
+                      true,
                     ),
                   ),
-                  padding: const EdgeInsets.only(
-                      left: 24.0, right: 24.0, top: 10, bottom: 10),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.notifications,
-                        color: Colors.black12,
-                        size: 40,
-                      ),
-                      const SizedBox(width: 17.0),
-                      Container(
-                        child: const Text(
-                          "Notification",
-                          style: TextStyle(
-                            fontSize: 17.0,
-                            fontWeight: FontWeight.w500,
-                          ),
+                  ProfileMenu(
+                    "images/notif.png",
+                    "Notifications",
+                    false
+                  ),
+                  GestureDetector(
+                    onTap: (){
+                      // Get.offAll(() => EditRestrictionPage(), transition: Transition.fade);
+                    },
+                    child: ProfileMenu(
+                      "images/foodres.png",
+                      "Food Restriction",
+                      false
+                    ),
+                  )
+                  // Add more list tiles here
+                ],
+              ),
+              // SizedBox(height: 70),
+              Container(
+              padding: const EdgeInsets.only(top: 70),
+              child: GestureDetector(
+                onTap: () {
+                  Get.dialog(
+                    AlertDialog(
+                      title: const Text("Log Out"),
+                      content: const Text("Are you sure you want to log out?"),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Get.back();
+                          },
+                          child: const Text("No"),
                         ),
-                      ),
-                      const Spacer(),
-                      Container(
-                        child: const Icon(Icons.arrow_forward_ios,
-                            size: 17, color: Color(0xFF5F5F5F)),
-                      )
-                    ],
+                        TextButton(
+                          onPressed: () {
+                            Get.offAll(() => LoginPage(), transition: Transition.fade);
+                            // AuthController.to.logout();
+                          },
+                          child: const Text("Yes"),
+                        ),
+                      ],
+                    ),
+                  );
+                  // AuthController.to.logout();
+                },
+                child: const Text(
+                  "Log Out",
+                  style: TextStyle(
+                    color: Color(0xFFD13D34),
+                    fontSize: 15,
+                    fontFamily: "Rubik",
+                    fontWeight: FontWeight.w400,
                   ),
                 ),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(
-                        color: Colors.black12,
-                        width: 1.0,
-                      ),
-                      // top: BorderSide(
-                      //   color: Colors.black12,
-                      //   width: 1.0,
-                      // ),
-                    ),
-                  ),
-                  padding: const EdgeInsets.only(
-                      left: 24.0, right: 24.0, top: 10, bottom: 10),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.analytics,
-                        color: Colors.black12,
-                        size: 40,
-                      ),
-                      const SizedBox(width: 17.0),
-                      Container(
-                        child: const Text(
-                          "Order’s Recapitulation",
-                          style: TextStyle(
-                            fontSize: 17.0,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ),
-                      const Spacer(),
-                      Container(
-                        child: const Icon(Icons.arrow_forward_ios,
-                            size: 17, color: Color(0xFF5F5F5F)),
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            )),
-            Spacer(),
-            Container(
-                padding: EdgeInsets.only(bottom: 50),
-                child: GestureDetector(
-                  onTap: () {
-                    // TODO: show modal to confirm logout
-                    Get.find<AuthController>().logout();
-                    Get.offAll(() => LoginPage());
-                  },
-                  child: Text(
-                    "Log Out",
-                    style: TextStyle(
-                      color: Color(0xFFD13D34),
-                      fontSize: 15,
-                      fontFamily: "Rubik",
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                )),
-          ],
-        ));
+              )),
+              
+            ],
+          ),
+        ],
+      ),
+      bottomNavigationBar: Container(   
+            height: 87.7,                                          
+            decoration: BoxDecoration(          
+                                                   
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(25.50), topRight: Radius.circular(25.50), bottomLeft: Radius.circular(0), bottomRight: Radius.circular(0), ),
+        
+                boxShadow: [                                                               
+                BoxShadow(color: Color.fromRGBO(0,0,0,230), spreadRadius: 0, blurRadius: 20),       
+                ],
+
+            ),               
+
+            child: ClipRRect( 
+                
+                                                                         
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(25.50), topRight: Radius.circular(25.50), bottomLeft: Radius.circular(0), bottomRight: Radius.circular(0), ),
+                                                                   
+                child:CustomBottomNavigationBar(initialIndex: 3),
+            )
+      )
+    );
   }
 }
