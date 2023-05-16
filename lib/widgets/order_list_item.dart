a16b1ab7525b507f49ba3cd25e3a695400e53acd
unknown <sanialubba10@gmail.com>
2023-05-16 16:03:10 +0700

    order page merchant

---
+++ lib/widgets/order_list_item.dart
@@ -0,0 +1,59 @@
+import 'dart:js_util';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class OrderListItem extends StatelessWidget {
+  final String quantity;
+  final String item;
+  final String price;
+
+  const OrderListItem(this.quantity, this.item, this.price);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 15, right: 15, bottom: 5, top: 5),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Text(
+              quantity,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 12,
+                fontFamily: "Rubik",
+              ),
+              textAlign: TextAlign.start,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Text(
+              item,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 12,
+                fontFamily: "Rubik",
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Text(
+              price,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 12,
+                fontFamily: "Rubik",
+              ),
+              textAlign: TextAlign.end,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/restriction_page_type.dart
@@ -0,0 +1,58 @@
+//template
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class RestrictionType extends StatelessWidget {
+  double sizetext = 10;
+  Map<String, dynamic> style = {
+    "MEAT-FREE": BoxDecoration(
+      borderRadius: BorderRadius.circular(8),
+      color: Color(0xffffd2dc),
+    ),
+    "DAIRY-FREE": BoxDecoration(
+      borderRadius: BorderRadius.circular(8),
+      color: Color(0xffd8f2ee),
+    ),
+    "NUT-FREE": BoxDecoration(
+      borderRadius: BorderRadius.circular(8),
+      color: Color(0xffe3d2b9),
+    ),
+    "EGG-FREE": BoxDecoration(
+      borderRadius: BorderRadius.circular(8),
+      color: Color(0xfffee7a2),
+    ),
+    "FISH-FREE": BoxDecoration(
+      borderRadius: BorderRadius.circular(8),
+      color: Color.fromARGB(255, 221, 196, 244),
+    ),
+    "GLLUTEN-FREE": BoxDecoration(
+      borderRadius: BorderRadius.circular(8),
+      color: Color.fromARGB(255, 228, 253, 215),
+    ),
+  };
+
+  final String title;
+  RestrictionType(this.title, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // width: 80,
+      //make width adjustable with text length
+
+      width: title.length * 7.0,
+      decoration: style[title],
+      padding: EdgeInsets.symmetric(horizontal: sizetext*0.2, vertical: 1),
+      child: Center(
+        child: Text(
+          title,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: sizetext,
+          ),
+        ),
+      ),
+    );
+  }
+}
